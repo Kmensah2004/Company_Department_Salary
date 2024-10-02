09942578f401cc205769180335193251e52b83e4
Kosi Mensah <kmensah@usf.edu>
2024-10-02 13:37:30 -0400

    Initialize company structure with departments and employees

---
+++ Company_Department_Salary.js
@@ -0,0 +1,52 @@
+const company = {
+ departments: [
+ {
+ departmentName: 'IT',
+ employees: [
+    {
+     name: 'Kosi',
+     salary: 105000,
+     subordinates: [
+     {
+     name: 'Dejalie',
+     salary: 80000,
+    subordinates: [
+     {
+     name: 'Eric',
+    salary: 60000,
+    subordinates: []
+     }
+     ]
+    }
+    ]
+    },
+    {
+    name: 'Riley',
+    salary: 50000,
+    subordinates: []
+                }
+            ]
+        },
+        {
+     departmentName: 'HR',
+     employees: [
+    {
+     name: 'Shiv',
+     salary: 85000,
+    subordinates: [
+    {
+         name: 'Anna',
+        salary: 75000,
+        subordinates: []
+     }
+         ]
+           },
+ {
+     name: 'Jen',
+     salary: 95000,
+     subordinates: []
+                }
+            ]
+        }
+    ]
+};
